ba2cc49de39cb949ba405d0aeebb4e383bebf415
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-03-28 08:29:14 +0100

    Fixed parsestring defect

--- src/parseString.js
+++ src/parseString.js
@@ -12,12 +12,12 @@ const data = (chunk, p) => {
 
   r = parser.data(chunk, p)
 
-  return r[0].length > 0 ? r : [parser.end(r[1], undefined)]
+  return r[0].length > 0 ? r : [parser.end(r[1]), undefined]
 }
 
 // parseString :: String -> Stream [String]
 const parseString = s => Stream(
-  data(s, parser.create(1)),
+  data(s, parser.create(1, 1)),
   state => state[0].length > 0 ? state[0][0] : Stream.EOS,
   state => data("", state[1])
 )
--- test/parseString.test.js
+++ test/parseString.test.js
@@ -10,6 +10,8 @@ describe("parser", function() {
   describe("#parseString", function() {
     it("should parse a string to stream", function() {
       assert.deepStrictEqual(parse('one, two\nhello"world,3.14'), [['one', ' two'], ['hello"world', '3.14']])
+      assert.deepStrictEqual(parse('one, two\nhello"world,3.14\n'), [['one', ' two'], ['hello"world', '3.14']])
+      assert.deepStrictEqual(parse('one, two\nhello"world,3.14\nGuy,Pratt'), [['one', ' two'], ['hello"world', '3.14'], ['Guy', 'Pratt']])
     })
   })
 })
